38ff0ae71f81b065dec85c313274a0603000f279
Note <note@note.com>
2020-06-12 18:27:24 -0300

    ssddfasdf

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { PI } from "@mattee37/npm-first-test";
 
 function App() {
   return (
     <>
       <h3>Hola!</h3>
       <h2>No se programar.</h2>
+      <h1>{PI}</h1>
     </>
   );
 }
