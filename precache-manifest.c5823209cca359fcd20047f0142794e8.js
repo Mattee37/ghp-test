422051ca2965e04dda4c5d7805520433ccf81f28
Note <note@note.com>
2020-06-12 10:55:37 -0300

    Updates

---
+++ precache-manifest.c5823209cca359fcd20047f0142794e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b00d8f181786d410635fe622e7e9b48",
+    "url": "/ghp-test/index.html"
+  },
+  {
+    "revision": "f47dc70d605d9f92bdd6",
+    "url": "/ghp-test/static/js/2.d98ac4a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ghp-test/static/js/2.d98ac4a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1adf67700077ce439437",
+    "url": "/ghp-test/static/js/main.466cd4ff.chunk.js"
+  },
+  {
+    "revision": "e73c2436778cb6e4bcc2",
+    "url": "/ghp-test/static/js/runtime-main.c12d122b.js"
+  }
+]);
